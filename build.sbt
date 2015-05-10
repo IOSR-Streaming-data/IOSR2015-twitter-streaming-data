5dd5494bba97336fddb85ec1e06dd6511bc10776
Mateusz Jaje <mateuszjaje@gmail.com>
2015-05-10 22:14:03 +0200

    added spark dependencies, renamed subproject from cass to cassandra for better meaning

--- build.sbt
+++ build.sbt
@@ -4,17 +4,18 @@ name := """IOSR-streaming-data"""
 lazy val commonSettings = Seq(
   organization := "pl.edu.agh.iosr.iosr2015.data.streaming.twitter",
   version := "0.1.0",
-  scalaVersion := "2.11.6"
+  scalaVersion := "2.11.6",
+  libraryDependencies ++= Dependencies.sparkAkkaHadoop
 )
 
 lazy val core = project
   .settings(commonSettings: _*)
 
-lazy val cass = project.dependsOn(core)
+lazy val cassandra = project.dependsOn(core)
   .settings(commonSettings: _*)
   .settings(
     libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0"
   )
 
-lazy val root = project.in(file(".")).aggregate(core, cass)
+lazy val root = project.in(file(".")).aggregate(core, cassandra)
   .settings(commonSettings: _*)
---
+++ project/Dependencies.scala
@@ -0,0 +1,40 @@
+import sbt._
+
+object Version {
+  val spark     = "1.3.1"
+  val hadoop    = "2.4.0"
+  val slf4j     = "1.7.6"
+  val logback   = "1.1.1"
+  val scalaTest = "2.1.7"
+  val mockito   = "1.9.5"
+  val akka      = "2.3.9"
+}
+
+object Library {
+  // workaround until 2.11 version for Spark Streaming's available
+  val sparkStreaming = "org.apache.spark"  %% "spark-streaming"         % Version.spark
+  val sparkTwitter   = "org.apache.spark"  %% "spark-streaming-twitter" % Version.spark
+  val akkaActor      = "com.typesafe.akka" %% "akka-actor"              % Version.akka
+  val akkaTestKit    = "com.typesafe.akka" %% "akka-testkit"            % Version.akka
+  val hadoopClient   = "org.apache.hadoop" %  "hadoop-client"           % Version.hadoop
+  val slf4jApi       = "org.slf4j"         %  "slf4j-api"               % Version.slf4j
+  val logbackClassic = "ch.qos.logback"    %  "logback-classic"         % Version.logback
+  val scalaTest      = "org.scalatest"     %% "scalatest"               % Version.scalaTest
+  val mockitoAll     = "org.mockito"       %  "mockito-all"             % Version.mockito
+}
+
+object Dependencies {
+
+  import Library._
+
+  val sparkAkkaHadoop = Seq(
+    sparkStreaming,
+    sparkTwitter,
+    akkaActor,
+    akkaTestKit,
+    hadoopClient,
+    logbackClassic % "test",
+    scalaTest % "test",
+    mockitoAll % "test"
+  )
+}
