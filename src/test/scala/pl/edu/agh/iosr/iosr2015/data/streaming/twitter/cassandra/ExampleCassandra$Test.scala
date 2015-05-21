eb456a732b19875d01c51430f8b02a2136153e66
Mateusz Jaje <mateuszjaje@gmail.com>
2015-05-21 10:47:52 +0200

    improved tests

--- src/test/scala/pl/edu/agh/iosr/iosr2015/data/streaming/twitter/cassandra/ExampleCassandra$Test.scala
+++ src/test/scala/pl/edu/agh/iosr/iosr2015/data/streaming/twitter/cassandra/ExampleCassandra$Test.scala
@@ -1,9 +1,13 @@
 package pl.edu.agh.iosr.iosr2015.data.streaming.twitter.cassandra
 
+import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.scalatest.{Matchers, OneInstancePerTest, WordSpec}
+import pl.edu.agh.iosr.iosr2015.data.streaming.twitter.cassandra.demos.ExampleCassandraApp._
+import pl.edu.agh.iosr.iosr2015.data.streaming.twitter.cassandra.demos.Foo
 
 import scala.collection.JavaConversions._
+import scala.util.Random
 
 class ExampleCassandra$Test extends WordSpec with OneInstancePerTest with Matchers {
 
@@ -11,9 +15,18 @@ class ExampleCassandra$Test extends WordSpec with OneInstancePerTest with Matche
 
   "it" should {
     "create tables" in {
-      val (sc, space: String, table: String) = ctx.initialize()
-      ctx.saveSomeData(sc, space, table)
-      CassandraConnector(sc.sc.getConf).withSessionDo { session =>
+      val (scf, conf) = ctx.initialize()
+      val space = "testkeyspace"
+      val table = "testtable"
+      createNamespace(space, conf)
+      dropTable(space, table, conf)
+      createTable(space, table, "id" -> "text", List("word" -> "text", "cnt" -> "int"), conf)
+      val data = 1 to 100 map { i =>
+        Foo(s"id$i", Random.nextLong(), Random.alphanumeric.take(10).mkString)
+      }
+      scf.sc.parallelize(data).saveToCassandra(space, table)
+
+      CassandraConnector(conf).withSessionDo { session =>
         session.execute(s"select * from $space.$table;").iterator().toList.length should equal(10)
       }
     }
